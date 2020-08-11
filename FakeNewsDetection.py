1e3139d6b72c7a9643acc40b5ccde3b1a8aa4018
Dr.Strange <54847725+ashish807@users.noreply.github.com>
2020-08-11 19:42:13 +0545

    Fake News Detection

---
+++ FakeNewsDetection.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+dataframe =pd.read_csv("news.csv")
+
+print(dataframe.shape)
+dataframe.head(5)
+labels =dataframe.label
+print(labels.shape)
+X_train,X_test,y_train,y_test =train_test_split(dataframe["text"],labels,test_size=0.2,random_state=7)
+print(X_train.shape)
+print(y_train.shape)
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english',max_df=0.7)
+tfidf_train=tfidf_vectorizer.fit_transform(X_train)
+tfidf_test = tfidf_vectorizer.transform(X_test)
+
+pac=PassiveAggressiveClassifier(max_iter=50)
+
+print(tfidf_train.shape)
+
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+confusion_matrix(y_test,y_pred,labels=['FAKE','REAL'])
